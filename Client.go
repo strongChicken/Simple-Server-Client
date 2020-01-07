5347c0e1c7030310dda060b88c4012c01fc352c4
Water <water@WaterdeiMac.local>
2020-01-07 11:40:11 +0800

    []int to byte

--- Client.go
+++ Client.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -34,20 +36,20 @@ func main()  {
 func handleRead(conn net.Conn, s *sync.WaitGroup) {
 	defer s.Done()
 
-	reader := bufio.NewReader(conn)
-	for i := 1; i < 10; i++{
-		line , err := reader.ReadString(byte('\n'))
-		if err != nil{
-			fmt.Printf("error: %v\n", err)
-			return
-		}
-		fmt.Printf(line)
+	reader, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil{
+		fmt.Printf("Error of Read : %v\n", err)
+		return
 	}
+		fmt.Printf(reader)
 }
 
 func handleWrite(conn net.Conn, s *sync.WaitGroup) {
 	defer s.Done()
 
+	ArrayData := [5]int{1, 2, 3, 4, 5}
+	ByteArrayData = IntToByte(ArrayData)
+	conn.Write([]byte )
 	for i := 10; i > 0; i--{
 		_, err := conn.Write([]byte("hello" + strconv.Itoa(i) + "\r\n"))
 
@@ -56,4 +58,21 @@ func handleWrite(conn net.Conn, s *sync.WaitGroup) {
 			break
 		}
 	}
+}
+
+func IntToByte(n int) []byte {
+	x := int(n)
+
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.LittleEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+func ByteToInt(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+
+	var x int
+	binary.Read(bytesBuffer, binary.LittleEndian, &x)
+
+	return int(x)
 }
